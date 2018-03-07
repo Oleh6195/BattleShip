923a4c2319ddbc56331516a52afa84c076e9a8ac
Oleh6195 <35238166+Oleh6195@users.noreply.github.com>
2018-03-07 18:45:01 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,11 +1,18 @@
 class Ship:
+    """
+    This class represents ship and his methods
+    """
     def __init__(self, bow, horizontal, lenght, hit):
-        self.bow = bow[:]
+        self.bow = bow
         self.horizontal = horizontal
         self.length = lenght
         self.hit = hit[:]
 
     def get_cordanates(self):
+        """
+        Makes list with cordinates all point of ship
+        :return:
+        """
         letters = "ABCDEFGHIJ"
         cordinates = []
         if self.horizontal:
@@ -13,9 +20,14 @@ class Ship:
                 cordinates.append((self.bow[0], self.bow[1] + point))
         elif not self.horizontal:
             for point in range(1, max(self.length)):
-                cordinates.append(letters[letters.index(self.bow[0]) + point],
-                                  self.bow[1])
+                cordinates.append((letters[letters.index(self.bow[0]) + point],
+                                  self.bow[1]))
         return cordinates
 
     def shoot_at(self, cordinates):
-        tuple(list(self.hit).append(cordinates))
+        """
+        Shoot to point of ship
+        :param cordinates:
+        :return:
+        """
+        self.hit.append(cordinates)
