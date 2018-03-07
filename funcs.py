3239932a75784c8f6d33aa81e6548084ee07b121
Oleh6195 <35238166+Oleh6195@users.noreply.github.com>
2018-03-07 18:43:08 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,11 +1,8 @@
-import random
-
-
 def read_field(path):
     """
     (str) -> (data)
     :param path:
-    :return:
+    :return: read file with
     """
     letters = 'ABCDEFGHIJ'
     field = {}
@@ -25,12 +22,18 @@ def read_field(path):
 def has_ship(field, cordinates):
     """
     (data, tuple) -> (bool)
-    :return:
+    :return: True if this cordinate has ship and False if it hasn't
     """
     return True if field[cordinates[0]][cordinates[1]-1] == '*' else False
 
 
 def ship_size(data, cordinates):
+    """
+    (dict, tuple) -> (tuple)
+    :param data:
+    :param cordinates:
+    :return: tuple with size of ship
+    """
     letters = 'ABCDEFGHIJ'
     size = 1
     position = cordinates[1] - 1
@@ -77,6 +80,12 @@ def ship_size(data, cordinates):
 
 
 def is_valid(data):
+    """
+    (dict) -> (bool)
+    This function check whether field is validate
+    :param data:
+    :return:
+    """
     size_lst = []
     for key in data:
         for point in range(len(data[key])):
@@ -105,15 +114,13 @@ def is_valid(data):
 def field_to_str(data):
     """
     (data) -> (str)
-    This function converts a field in list of lists format(matrix)
-    to string format
+    This function converts a field in dictionary to string format
     """
     line = "        -----|-----|-----|-----|-----|-----|--" \
            "---|-----|-----|-----\n"
     nums_str = "          1     2     3     4     5     6  " \
                "   7     8     9     10     \n"
     field_str = ""+nums_str+line
-    letters = "ABCDEFGHIJ"
     for key in data:
         field_str += "    "+key+"  |"
         for j in range(1, len(data[key])):
@@ -121,92 +128,3 @@ def field_to_str(data):
         field_str += "\n"
         field_str += line
     return field_str
-
-
-def generate():
-    field, ship = {}, {4: [1], 3: [1, 1], 2: [1, 1, 1], 1: [1, 1, 1, 1]}
-    direction, letters = "vh", 'ABCDEFGHIJ'
-    for letter in letters:
-        field[letter] = []
-        for i in range(10):
-            field[letter].append(' ')
-        field[letter].append('\n')
-    for key in ship:
-        while ship[key] != []:
-            cordinates = ()
-            while cordinates == ():
-                y = random.choice(letters)
-                x = random.randint(0, 9)
-                direc = random.choice(direction)
-                horizontal = field[y]
-                if y == "A":
-                    hor_down = field[letters[letters.index(y) + 1]]
-                    hor_up = []
-                elif y == "J":
-                    hor_up = field[letters[letters.index(y)-1]]
-                    hor_down = []
-                else:
-                    hor_up = field[letters[letters.index(y) - 1]]
-                    hor_down = field[letters[letters.index(y) + 1]]
-                vertical = [field[key][x] for key in field]
-                vertical_up = [field[key][x-1] for key in field]
-                vertical_down = [field[key][x+1] for key in field]
-                if direc == "h":
-                    if '*' not in field[y][x: x + 2 + key] and x <= 10 - key:
-                        try:
-                            work1 = horizontal[:x]
-                            work2 = horizontal[x + 1:]
-                            if work1[-1] == "*":
-                                continue
-                            elif work2[0] == "*":
-                                continue
-                            wor1 = hor_up[x-1:x+key+1]
-                            wor2 = hor_down[x-1:x+key+1]
-                            if "*" in wor1:
-                                continue
-                            if "*" in wor2:
-                                continue
-                            else:
-                                cordinates = (x, y)
-                        except IndexError:
-                            pass
-                if direc == "v":
-                    if "*" not in vertical[letters.index(y):
-                                           letters.index(y) + 2 + key] and \
-                                           letters.index(y) <= 10 - key:
-                        try:
-                            work1 = horizontal[:x]
-                            work2 = horizontal[x + 1:]
-                            if work1[-1] == "*":
-                                continue
-                            elif work2[0] == "*":
-                                continue
-                            wor1 =\
-                                vertical_up[letters.index(y) -
-                                            1:letters.index(y)+key+1]
-                            wor2 = vertical_down[letters.index(y) -
-                                                 1:letters.index(y)+key+1]
-                            if "*" in wor1:
-                                continue
-                            if "*" in wor2:
-                                continue
-                            else:
-                                cordinates = (x, y)
-                        except IndexError:
-                            continue
-            if direc == "v":
-                ship[key].remove(1)
-                for keys in range(letters.index(y), letters.index(y) + key):
-                    field[letters[keys]][x] = "*"
-            elif direc == "h":
-                ship[key].remove(1)
-                for elements in range(x + 1, x + 1 + key):
-                    field[y][elements] = '*'
-    return field
-
-
-def generate_field():
-    field = generate()
-    while not is_valid(field):
-        field = generate()
-    return field
